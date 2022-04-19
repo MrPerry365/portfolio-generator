095d7b214db7a1e985599a2cf00d935213def65c
MrPerry365 <1987nowhere@gmail.com>
2022-04-19 17:15:47 -0400

    intro to Node.js, module 9.1

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello Node!);
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // this...
+    for (let i = 0; i < profileDataArr.length; i++)
+    {
+        console.log(profileDataArr[i]);
+    }
+    console.log('=====================');
+
+    // Is the same as this....
+    profileDataArr.forEach(profileItem => 
+        console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
