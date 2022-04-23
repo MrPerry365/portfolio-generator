3258b573e84bbd2658b0604b19bd6587c7c175bc
MrPerry365 <1987nowhere@gmail.com>
2022-04-23 13:54:45 -0400

    commit befor modularizing the code

--- app.js
+++ app.js
@@ -1,86 +1,30 @@
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-// // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-// const generatePage = (userName, githubName) => {
-//     return `
-//     Name: ${userName}
-//     Github: ${githubName}
-//     `;
-// };
-// console.log(generatePage(profileDataArgs[0], profileDataArgs[1]));
+const fs = require('fs');
 
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
 
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-        type:"input",
-        name:'firstName', 
-        message:'What is your first name?',
-    }
-  ])
-  .then((answers) => {
-      console.log(answers);
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-      console.error(error);
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-  });
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
