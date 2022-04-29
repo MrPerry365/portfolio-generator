e3fd8a948f21205bce8d628cc2d1afaef3e7a801
MrPerry365 <1987nowhere@gmail.com>
2022-04-29 14:29:43 -0400

    add new promise for writeFile and copyFile

---
+++ utils/generate-site.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContenet, err => {
+            //if there's an error, reject the Promise, and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'file created!'
+            });
+        });
+      });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'Stylesheet created!'
+            });
+        });
+    });
+};
