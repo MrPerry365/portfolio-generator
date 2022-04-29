6568a2bc37d0875621df95d67eaac1e81b739082
MrPerry365 <1987nowhere@gmail.com>
2022-04-29 15:08:37 -0400

    fin

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 
+// writing files
 const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/index.html', fileContenet, err => {
-            //if there's an error, reject the Promise, and send the error to the Promise's `.catch()` method
-            if (err) {
-                reject(err);
-                return;
-            }
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-            resolve({
-                ok: true,
-                message: 'file created!'
-            });
-        });
+      resolve({
+        ok: true,
+        message: 'File created!'
       });
-};
-
-const copyFile = () => {
-    return new Promise((resolve, reject) => {
-        fs.copyFile('./src/style.css', './dist/style.css', err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            resolve({
-                ok: true,
-                message: 'Stylesheet created!'
-            });
-        });
     });
+  });
 };
 
-module.exports = {
-    writeFile, copyFile};
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
+    });
+  });
+};
+
+module.exports = { writeFile, copyFile };
